2a8a5780eba0864b7af171e90531229c81379fb6
José Ángel <jnune111@gmail.com>
2025-06-19 15:54:44 -0400

    particionamiento de la nav

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -12,29 +12,7 @@
 </head>
 <body>
     <header>
-        <div class="container">
-            <nav>
-                <div class="contenedor">
-                    <img src="{{asset('Logo_UDO.png')}}" class="logo">
-                </div>
-                <div >
-                    <img src="{{asset('tituloLogo.png')}}" class="logo_barra">
-                </div>
-                    @auth
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-                            <button type="submit" class="cerrar_sesion">
-                                Cerrar Sesión
-                            </button>
-                        </form>
-                    @endauth
-                    @guest
-                        <a href="{{route('login')}}" class="button_barra">Iniciar sesion</a>
-                    @endguest
-                
-                
-            </nav>
-        </div>
+        <x-panel_nav></x-panel_nav>
     </header>
 
     <main>
